4fc37850d7e7e2f6ffef88de7d07be14e51d586f
EMANUEL SANTOS MARINHO SALES <021.898672@senaicimatec.edu.br>
2024-04-22 17:58:17 -0300

    info2 e mudanças css

--- src/components/Info2.js
+++ src/components/Info2.js
@@ -10,16 +10,22 @@ function Infodois() {
         <>
         <div class="mulher-conteiner">
             <div>
-                <h5>Comunique-se</h5>
-
+                <h5 class="mulher-title">Comunique-se</h5>
+                <div class="card-mulher">
+                    <h1 class="card-titulo-mulher">Nosso objetivo é empoderar você, fornecendo informações claras e acessíveis para que possa tomar decisões informadas e defender seus interesses.
+O fórum do consuweb é um espaço vibrante e interativo onde consumidores de todos os lugares podem se reunir para compartilhar experiências e obter apoio. É um lugar onde as dúvidas são esclarecidas e as informações são compartilhadas, ajudando a criar uma comunidade de consumidores conscientes e informados.</h1>
+                </div>
+                <div>
+                    <h5 class="mulher-title-dois">Empodere-se</h5>
+                </div>
             </div>
-
-
+            
         </div>
+
         
         <div>
                 <img src={imgMulher} className="imagem-mulher" />
-
+                
             </div>
         </>
     );
