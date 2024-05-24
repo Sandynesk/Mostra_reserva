9287ede74da53750a254ce63e5b2597da806f791
ANAILA MATOS SILVA <021.900239@senaicimatec.edu.br>
2024-05-24 07:35:10 -0700

    colocando o formulario no meio

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,62 +1,69 @@
+//comentar ccs
+// descer a box 
+// colocar foto
+
 import React from 'react';
-import './Css/Login.css'
 import { Button, Form } from 'react-bootstrap';
-import Fotologin from '../Fotos/Fotologin.jpg';
+import './Css/Login.css';
+import teste1 from '../Fotos/teste1.jpg';
 
 
 
 const Login = () => {
 
-  const handleLogin = (e) => { //função  que e chamada qnd o user clica em logar 
-
-    e.preventDefault(); // isso aqui impede que, ao enviar o formulario (clicando em logar), a pg recarregue
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Lógica de autenticação aqui           
   };
 
+  return (
 
-  return ( //estrutura da pg comeca aqui
-      
-  
-  <div className="login-container">
-      <img src={Fotologin} alt="fotodeladinho" className="fotodeladinho"></img>
-      <h2>Entre com sua conta</h2>  
-      
-      <Form onSubmit={handleLogin}> {/* qnd o user clicar em enviar esse formulario ira chamar a função handlelogin */}
+
+      <div className="login-form">
+        <h2>Bem vindo de volta!</h2>
         
-        <div className='input-box'>
+        <Form onSubmit={handleLogin}>
+          
+          <div className='input-box'>
 
-<Form.Group controlId="formBasicEmail"> {/* grupo de formulario da biblioteca REACT que agrupa o form de email */}
-        {/*   <Form.Label>Email</Form.Label> */}
-          <Form.Control type="email" placeholder="Seu email" /> {/* campo de entrada para o email do user */}
-        </Form.Group>
-        </div>
+    <Form.Group controlId="formBasicEmail">
+  
+            <Form.Control type="email" placeholder="Email" />
+          </Form.Group>
+          </div>
 
-        <div className='input-box'>
-        <Form.Group controlId="formBasicPassword">
-          {/* <Form.Label>Senha</Form.Label> */}
-          <Form.Control type="password" placeholder="Senha" />
-        </Form.Group>
-        </div>
+          <div className='input-box'>
+          <Form.Group controlId="formBasicPassword">
+  
+            <Form.Control type="password" placeholder="Senha" />
+          </Form.Group>
+          </div>
 
-        <div className='lembrar-senha'>
-          <label><input type='checkbox'/> Lembrar</label>
-          <a className='style-link' href='#'> Esqueceu a senha?</a>
-        </div>
+          <div className='remember-forgot'>
+            <label><input type='checkbox'/> Lembrar</label>
+            <a href='#'> Esqueceu a senha?</a>
+          </div>
 
-        <Button variant="primary" type="submit" block className="custom-button">
-Entrar
-</Button>
-
-
-        < div className='register-link'>
-          <p> Não tem uma conta? <a className='style-link' href="/register">Registre-se</a></p>
-        </div>
+          <Button variant="primary" type="submit" block className="custom-button">
+    Entrar
+    </Button>
 
 
+          < div className='register-link'>
+            <p> Não tem uma conta? <a href="/cadastro">Cadastre-se</a></p>
+          </div>
 
 
-      </Form>
-    </div>
+
+
+        </Form>
+      </div>
+
+
+
   );
-}
+};
+
 
 export default Login;
+
