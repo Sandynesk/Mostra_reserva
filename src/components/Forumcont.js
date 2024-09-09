592e2708810f25d9da19ca62c7c771493a5211ad
CARLOS EDUARDO TEIXEIRA GUIA <021.898687@senaicimatec.edu.br>
2024-09-09 14:28:52 -0300

    Fórum esqueleto feito

---
+++ src/components/Forumcont.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Css/Forumcont.css';
+
+function Forumcont(){
+
+    <body>
+        <div class="container">
+            <h1>Fórum Simples</h1>
+            <div class="message-form">
+                <h2>Nova Mensagem</h2>
+                <form id="messageForm">
+                    <textarea id="messageContent" placeholder="Digite sua mensagem..."></textarea>
+                    <button type="button" onclick="postMessage()">Enviar</button>
+                </form>
+            </div>
+            <div id="messageBoard">
+            </div>
+        </div>
+    </body>
+
+}
+
+export default Forumcont;
