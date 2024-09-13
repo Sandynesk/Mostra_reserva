f923616c3f329ea1553d831c4baaf800257d9190
CARLOS EDUARDO TEIXEIRA GUIA <021.898687@senaicimatec.edu.br>
2024-09-13 14:26:46 -0300

    forumcoisas

--- src/components/Forumcont.js
+++ src/components/Forumcont.js
@@ -4,19 +4,31 @@ import './Css/Forumcont.css';
 function Forumcont(){
     return (
             
-    <body>
-    <div class="container">
+        <body>
+        <header>
             <h1>Fórum ConsuWeb</h1>
-            <div class="message-form">
-                <form id="messageForm">
-                    <textarea id="messageContent" placeholder="Digite sua mensagem..."></textarea>
-                    <button type="button" onclick="postMessage()">Enviar</button>
-                </form>
-            </div>
-            <div id="messageBoard">
-            </div>
-        </div>
+        </header>
+        <main>
+            <section class="new-post">
+                <h2>Crie um novo post</h2>
+                <textarea placeholder="Escreva seu post aqui..."></textarea>
+                <button>Postar</button>
+            </section>
+            <section class="posts">
+                <article>
+                    <h2><a href="post">Título do Post 1</a></h2>
+                    <p>Conteúdo do post 1... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel dignissim nisl.</p>
+                    <p><a href="post">Leia mais</a></p>
+                </article>
+                <article>
+                    <h2><a href="post">Título do Post 2</a></h2>
+                    <p>Conteúdo do post 2... Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p><a href="post">Leia mais</a></p>
+                </article>
+            </section>
+        </main>
     </body>
+    
     )
 
 }
