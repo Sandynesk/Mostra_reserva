78ee9323d5020959a8c98abd985c47d235dd1a1f
CARLOS EDUARDO TEIXEIRA GUIA <021.898687@senaicimatec.edu.br>
2024-10-16 16:02:40 -0300

    sim, o FAQ

--- src/components/FAQ/FaqHome.js
+++ src/components/FAQ/FaqHome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FaqNavbar from './FaqJS/FaqNavbar.js';
+import FaqReal from './FaqJS/FaqReal.js'
 import Footer from '../Footer';
 
 
@@ -8,6 +9,7 @@ function FaqHome() {
       <div className="faq-container">
         
         <FaqNavbar /> {/* Renderize o componente Navbar */}
+        <FaqReal />
         <Footer /> {/* Renderize o componente Footer */}
 
       </div>
